fd34adb32b75bf63cc9f0bef617c1b18c3f4a0fe
JingZheng <song.jingz@husky.neu.edu>
2020-07-27 20:49:40 -0700

    more graphql types

--- schema/schema.js
+++ schema/schema.js
@@ -1,23 +1,86 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLFloat, GraphQLSchema } = graphql;
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLFloat,
+    GraphQLID,
+    GraphQLSchema
+} = graphql;
 
 var foods = [
-    { id: '1', name: 'banana', calories: 300 },
-    { id: '2', name: 'chicken', calories: 1000 },
-    { id: '3', name: 'steak', calories: 2000 }
+    { id: '1', name: 'banana', calories: 300, userId: '2' },
+    { id: '2', name: 'chicken', calories: 1000, userId: '2' },
+    { id: '3', name: 'steak', calories: 2000, userId: '1' },
+    { id: '4', name: 'fish', calories: 700, userId: '1' },
+    { id: '5', name: 'milk', calories: 100, userId: '2' },
+    { id: '6', name: 'burger', calories: 900, userId: '1' }
+];
+
+var exercises = [
+    { id: '1', name: 'basketball', calories: 500, userId: '1' },
+    { id: '2', name: 'lifting', calories: 600, userId: '1' },
+    { id: '3', name: 'tennis', calories: 800, userId: '2' },
+    { id: '4', name: 'swimming', calories: 900, userId: '1' },
+    { id: '5', name: 'boxing', calories: 1200, userId: '2' },
+];
+
+var users = [
+    { id: '1', name: 'jingzheng' },
+    { id: '2', name: 'menglin' }
 ]
 
 const FoodType = new GraphQLObjectType({
     name: 'Food',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
-        calories: { type: GraphQLFloat }
+        calories: { type: GraphQLFloat },
+        status: { type: GraphQLString },
+        user: {
+            type: UserType,
+            resolve(parent, args) {
+                console.log(parent);
+                return _.find(users, { id: parent.userId });
+            }
+        }
     })
 });
 
+const ExerciseType = new GraphQLObjectType({
+    name: 'Exercise',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        status: { type: GraphQLString },
+        user: { type: GraphQLString },
+        calories: { type: GraphQLFloat },
+        due: { type: GraphQLString },
+        user: {
+            type: UserType,
+            resolve(parent, args) {
+                console.log(parent);
+                return _.find(users, { id: parent.userId });
+            }
+        }
+    })
+});
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        exercise: {
+            type: ExerciseType,
+            resolve(parent, args) {
+                console.log(parent);
+                return _.find(exercises, { name })
+            }
+        }
+    })
+});
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
@@ -27,6 +90,13 @@ const RootQuery = new GraphQLObjectType({
             args: { name: { type: GraphQLString } },
             resolve(parent, args) {
                 return _.find(foods, { name: args.name });
+            }
+        },
+        exercise: {
+            type: ExerciseType,
+            args: { name: { type: GraphQLString } },
+            resolve(parent, args) {
+                return _.find(exercises, { name: args.name });
             }
         }
     }
